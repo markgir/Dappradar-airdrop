21ae5a65411e318424f9af27247279301f2c1954
Yogs <yogazacty@gmail.com>
2021-05-09 00:38:17 +0700

    remove old files

--- old/config.js
+++
@@ -1,7 +0,0 @@
-require('dotenv').config()
-module.exports = {
-    botToken: process.env.botToken,
-    channelId : process.env.channelId,
-    dappradarAuthorization : process.env.dappradarAuthorization,
-    airdropLogFile: process.env.logFile || 'lastAirdrop.json'
-}
